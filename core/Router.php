4ce0099718898ba473410e7d78c8d4a97d207c5e
valerahorsharik <valera.horsharik@gmail.com>
2017-02-28 18:22:26 +0200

    Create a router.

---
+++ core/Router.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace core;
+
+class Router
+{
+    /**
+     * Available routes
+     * 
+     * @var array
+     */
+    private $routes;
+    
+    /**
+     *  Current uri from request
+     * 
+     * @var string
+     */
+    private $uri;
+    
+    
+    public function __construct() {
+        $this->routes = $this->getRoutes();
+        $this->uri = $this->getURI();
+        $this->run();
+    }
+    
+    /**
+     * Determines controller,action, params
+     * and call createController method
+     */
+    protected function run(){
+                $programRoute = $this->findProgramRoute();
+                $parts = explode('/', $programRoute);
+                $controller = "controllers\\" .  ucfirst(array_shift($parts) . "Controller");
+                $action = lcfirst(array_shift($parts));
+                $params = $parts;
+                echo $controller . " " . $action ;
+                $this->createController($controller,$action,$params);
+    }
+    
+    /**
+     * Create a controller and call an $action
+     * with transmitted parameters
+     * 
+     * @param string $controller
+     * @param string $action
+     * @param array $params
+     * 
+     */
+    protected function createController($controller,$action,$params) {
+        $object = new $controller();
+        $object->$action($params);
+    }
+    
+    /**
+     * Find a program route, which will
+     * consist of  controller,action and params
+     * 
+     * @return array
+     */
+    protected function findProgramRoute(){
+        foreach ($this->routes as $uriPattern => $path) {
+            if (preg_match("~$uriPattern~", $this->uri)) {
+                $programRoute = preg_replace("~$uriPattern~", $path, $this->uri);
+                return $programRoute;
+            }
+        }
+    }
+    /**
+     * Get an URI from request
+     * @return string
+     */
+    protected function getURI(){
+        return substr(trim($_SERVER['REQUEST_URI']), 1);
+    }
+
+    /**
+    * Get all available routes from config/routes.php
+    * @return array
+    */
+    protected function getRoutes() {
+        return require_once('config/routes.php');       
+    }
+}

b3612121896f964385a8fe00b0ce214cdd6464d5
valerahorsharik <valera.horsharik@gmail.com>
2017-03-02 12:10:55 +0200

    Added a test controller.

---
+++ controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace controllers;
+
+use core\Controller as Controller;
+
+class HomeController extends Controller{
+    
+    public function index() {
+        echo "123";
+    }
+}

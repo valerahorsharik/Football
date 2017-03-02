29d2383ed5ed057fc4582acfb20f77ca8407463e
valerahorsharik <valera.horsharik@gmail.com>
2017-03-02 12:06:10 +0200

    Created basic controller.

---
+++ core/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace core;
+
+
+class Controller {
+   
+    /**
+     * Title of the page
+     * 
+     * @var string
+     */
+    protected $title = "Main Page";
+    
+    public function __construct() {
+        
+    }
+    
+    /**
+     * 
+     * Display $view with $data
+     * 
+     * @param string $view
+     * @param array $data
+     */
+    public function display($view,$data){
+        
+    }
+}

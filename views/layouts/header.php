4ba76ff1867d8f6e2df7996d9885e87fae0882ca
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 10:22:29 +0200

    Customize.

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,13 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="favicon.ico">
 
-    <title>Starter Template for Bootstrap</title>
+    <title><?= $this->title ?></title>
     <link href="public/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="public/css/main.css" rel="stylesheet">
@@ -15,26 +14,13 @@
   </head>
 
   <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
+      <div class="header">
+          <div class="container">
+              <div class="col-md-2 text-left" id="logo">
+                  <a  href="/">Football The Game</a>
+              </div>
+          </div>
       </div>
-    </nav>
+    
 
     <div class="container">

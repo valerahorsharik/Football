6869969504d1a7b00502a4518a401e8bc496704b
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 09:40:22 +0200

    testing with controller.

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -7,6 +7,6 @@ use core\Controller as Controller;
 class HomeController extends Controller{
     
     public function index() {
-        echo "123";
+        $this->view->display('default', ['a' => 7]);
     }
 }

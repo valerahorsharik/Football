62d217d37ab5b8f68bfcfad76186995ad2f03722
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 10:23:48 +0200

    Changed the version of jquery.

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,6 +1,5 @@
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
 
   </body>

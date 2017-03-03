2f78481c4dab25a2df604af754513e848c726d3d
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 15:05:24 +0200

    Developing registration with VK.

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -3,7 +3,19 @@
 namespace controllers;
 
 use core\Controller as Controller;
-
+use controllers\registrationWays\vk as vk;
 class RegistrationController extends Controller {
-   //put your code here
+   
+    /**
+     * Display login form.
+     */
+    public function loginForm() {
+        $this->view->display('registration/login');
+    }
+    
+    public function vk() {
+        $code = $_GET['code'];
+        $vk = new vk($code);
+    }
+
 }

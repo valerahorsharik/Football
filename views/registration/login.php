eb4b35866ee1e148cb859e62c6ae6e44ab5b1f85
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 15:11:05 +0200

    Added a login form.

---
+++ views/registration/login.php
@@ -0,0 +1,14 @@
+<div class="text-center">
+    Please, choose your way to login.
+</div>
+<div class="col-md-offset-4 social-way">
+    Via social: 
+    <a href="https://oauth.vk.com/authorize?client_id=5904978&display=popup&redirect_uri=http://pretty-simple.mcdir.ru/auth/social/vk/&scope=email&response_type=code&v=5.62">vk</a>
+    <a href="#">fb</a>
+</div>
+<div class="text-center">
+    or 
+</div>
+<div class="col-md-offset-4 default-way">
+    Via account:
+</div>

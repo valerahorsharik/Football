998f4a4ed011e43d94f13ae5183707c3e300a63b
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 09:42:35 +0200

    Display errors.

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once(__DIR__ . "/config/autoload.php");
 
+$router = new core\Router;

4242ea018123fd209dd72e1ae064e563e2cc1b22
valerahorsharik <valera.horsharik@gmail.com>
2017-03-03 16:26:45 +0200

    Now we can register user in DB.

--- controllers/registrationWays/vk.php
+++ controllers/registrationWays/vk.php
@@ -2,17 +2,9 @@
 
 namespace controllers\registrationWays;
 
+use core\Database as DB;
+
 class vk {
-//    $client_id = '5904978';
-//        $client_secret ='U0JEQEOJj9pqA50gSnOa';
-//        $redirect_uri ='http://pretty-simple.mcdir.ru/auth/social/' ;
-//        $code = $_GET['code'];
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_URL, "https://oauth.vk.com/access_token?client_id={$client_id}&client_secret={$client_secret}&redirect_uri={$redirect_uri}&code={$code}");
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-//        $_SESSION['access_token'] = curl_exec($ch);
-//        curl_close($ch);
-//        header('Location:/');
 
     /**
      *
@@ -60,18 +52,19 @@ class vk {
      * 
      * @var array
      */
-    private $necessaryData = ['email', 'user_id'];
+    private $necessaryData = ['email',
+        'user_id', 'first_name', 'last_name', 'photo_100'];
 
     public function __construct($code) {
         $this->getAccessToken($code);
     }
-    
-   /**
-    * 
-    * Get access token from VK
-    * 
-    * @param int $code
-    */
+
+    /**
+     * 
+     * Get access token from VK
+     * 
+     * @param int $code Contain temporary code for get access token
+     */
     private function getAccessToken($code) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://oauth.vk.com/access_token?client_id={$this->client_id}&client_secret={$this->client_secret}&redirect_uri={$this->redirect_uri}&code={$code}");
@@ -79,9 +72,24 @@ class vk {
         $answer = json_decode(curl_exec($ch));
         curl_close($ch);
         $this->setUsersData($answer);
-        $this->accessToken = $answer->access_token;
+        $this->getUsersDataWithToken($answer->access_token);
     }
-    
+
+    /**
+     * 
+     * Get all necessary data from VK 
+     * 
+     * @param int $token Contain an access token from VK
+     */
+    private function getUsersDataWithToken($token) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.vk.com/method/users.get?user_ids={$this->userData['user_id']}&fields=photo_100&access_token={$token}&v=5.62");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $answer = json_decode(curl_exec($ch));
+        curl_close($ch);
+        $this->setUsersData($answer->response[0]);
+    }
+
     /**
      * 
      * Set user data in accordance with $this->necessaryData
@@ -94,6 +102,21 @@ class vk {
                 $this->userData[$nameOfField] = $data->$nameOfField;
             }
         }
+    }
+
+    /**
+     * Check if the users allready in DB,
+     * if yes - just login,
+     * otherwise - register him and login
+     */
+    public function login() {
+        $stmt = DB::run("INSERT INTO users VALUES(NULL,?,?,?,?,?)", [
+                    $this->userData['first_name'],
+                    $this->userData['last_name'],
+                    $this->userData['email'],
+                    $this->userData['photo_100'],
+                    $this->userData['user_id']]);
+        
     }
 
 }

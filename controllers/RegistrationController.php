95723c3d1ec170300d7b29bd811869d2631b6503
valerahorsharik <valera.horsharik@gmail.com>
2017-03-05 16:51:18 +0200

    Create class for registration with FB.

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -4,6 +4,8 @@ namespace controllers;
 
 use core\Controller as Controller;
 use controllers\registrationWays\vk as vk;
+use controllers\registrationWays\vk as fb;
+
 class RegistrationController extends Controller {
    
     /**
@@ -14,6 +16,14 @@ class RegistrationController extends Controller {
         $code = $_GET['code'];
         $vk = new vk($code);
         $vk->tryToLogin();
+    }
+    
+    /**
+     * Login/registration via fb
+     * @see controllers\registrationWays\fb
+     */
+    public function fb() {
+        $fb = new $fb();
     }
      
 }

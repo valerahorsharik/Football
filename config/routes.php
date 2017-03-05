4d860a8ca706453422aa5639e610836585beff7e
valerahorsharik <valera.horsharik@gmail.com>
2017-03-05 18:30:54 +0200

    Added a route for the fb.

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@ return array(
     //Registration routes
 
     'auth/social/vk/*' => 'registration/vk/$1',
+    'auth/social/fb/*' => 'registration/fb/$1',
     
     //User's routes
     'logout' => 'user/logout',

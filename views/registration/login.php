ccad3a30616fab41212e5203b3b487ed871ea233
valerahorsharik <valera.horsharik@gmail.com>
2017-03-05 18:31:53 +0200

    Added a link for the FB.

--- views/registration/login.php
+++ views/registration/login.php
@@ -4,7 +4,7 @@
 <div class="col-md-offset-4 social-way">
     Via social: 
     <a href="https://oauth.vk.com/authorize?client_id=5904978&display=popup&redirect_uri=http://pretty-simple.mcdir.ru/auth/social/vk/&scope=email&response_type=code&v=5.62">vk</a>
-    <a href="#">fb</a>
+    <a href="https://www.facebook.com/v2.8/dialog/oauth?client_id=401003196941968&redirect_uri=http://pretty-simple.mcdir.ru/auth/social/fb/">fb</a>
 </div>
 <div class="text-center">
     or 

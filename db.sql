b95396a7ca5827e5dccabb8899be1757658ad2ba
valerahorsharik <valera.horsharik@gmail.com>
2017-03-06 12:18:33 +0200

    New version.

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 10.0.2.9
--- Время создания: Мар 03 2017 г., 17:29
+-- Время создания: Мар 06 2017 г., 11:05
 -- Версия сервера: 5.6.32-78.0-log
 -- Версия PHP: 5.4.45
 
@@ -28,19 +28,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `last_name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `photo` text CHARACTER SET utf8 NOT NULL,
-  `vk_id` int(11) DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=cp1251;
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `photo` text NOT NULL,
+  `vk_id` int(11) DEFAULT NULL,
+  `fb_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `photo`, `vk_id`) VALUES
-(1, 'Р’Р°Р»РµСЂР°', 'Р“РѕСЂС€Р°СЂРёРє', 'valera.horsharik@gmail.com', 'https://pp.userapi.com/c627428/v627428161/34d82/KXyok1X-Cnk.jpg', 11972161);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `photo`, `vk_id`, `fb_id`) VALUES
+(19, 'Валера', 'Горшарик', 'valera.horsharik@gmail.com', 'https://pp.userapi.com/c627428/v627428161/34d82/KXyok1X-Cnk.jpg', 11972161, NULL),
+(20, 'Валера', 'Горшарик', 'valera.horsharik@gmail.com', 'https://scontent.xx.fbcdn.net/v/t1.0-1/c15.0.50.50/p50x50/10354686_10150004552801856_220367501106153455_n.jpg?oh=0bb129c4bacce2fd26d99c098ed48ce3&oe=5938E12F', NULL, 2147483647);
 
 --
 -- Индексы сохранённых таблиц
@@ -60,7 +62,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

a9c4c979a3efd40188c2ecf72913516f32f89358
valerahorsharik <valera.horsharik@gmail.com>
2017-03-06 12:18:51 +0200

    Changed redirect uri's.

--- views/registration/login.php
+++ views/registration/login.php
@@ -3,8 +3,8 @@
 </div>
 <div class="col-md-offset-4 social-way">
     Via social: 
-    <a href="https://oauth.vk.com/authorize?client_id=5904978&display=popup&redirect_uri=http://pretty-simple.mcdir.ru/auth/social/vk/&scope=email&response_type=code&v=5.62">vk</a>
-    <a href="https://www.facebook.com/v2.8/dialog/oauth?client_id=401003196941968&redirect_uri=http://pretty-simple.mcdir.ru/auth/social/fb/&scope=email">fb</a>
+    <a href="https://oauth.vk.com/authorize?client_id=5904978&display=popup&redirect_uri=http://football.mcdir.ru/auth/social/vk/&scope=email&response_type=code&v=5.62">vk</a>
+    <a href="https://www.facebook.com/v2.8/dialog/oauth?client_id=401003196941968&redirect_uri=http://football.mcdir.ru/auth/social/fb/&scope=email">fb</a>
 </div>
 <div class="text-center">
     or 
